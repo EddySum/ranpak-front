d6e71ca7f02de11c3e6ea486ea4e8789893b565a
eddySum <adnansumra115@gmail.com>
2022-09-16 03:40:14 -0400

    basic auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth-guard/auth-guard.service';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+      path: '',
+      component: HomeComponent,
+      canActivate: [AuthGuard]
+  },
+  {
+      path: 'login',
+      component: LoginComponent
+  },
+
+
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/guards/auth-guard/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  isAuthenticated = false;
+
+  constructor(private router: Router) { }
+  
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if(!this.isAuthenticated) {
+      this.router.navigate(['/login']);
+    }
+    return this.isAuthenticated;
+}
+}
