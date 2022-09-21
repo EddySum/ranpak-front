d71e02755b671a23c341a4ac10bba520c4a1c011
eddySum <adnansumra115@gmail.com>
2022-09-21 15:27:02 -0400

    basic product card

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  product = {
+    "userId": "632949fe95a74e7fa7b79dff",
+    "productId": "123456789",
+    "name": "PS5",
+    "info": "This a test product. To learn more, ask for more. Yes, this is used for gaming",
+    "referenceNumber": 123,
+    "country": "USA",
+    "_id": "632b376cf38ed2be94031bae",
+    "createdAt": "2022-09-21T16:10:20.994Z"
+  }
 
   constructor() { }
 
